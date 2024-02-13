be7d066733b0fa15ddb4c4178d7b75864dc7aa11
NarminGuliyeva <narminguliyeva01@mail.ru>
2024-02-13 15:43:10 +0400

    commit2

--- aurum/src/Components/Counter.js
+++ aurum/src/Components/Counter.js
@@ -1,42 +1,40 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import CountUp from 'react-countup'
+import ScrollTrigger from 'react-scroll-trigger'
 
 function Counter() {
-    const [counter, setCounter] = useState(0);
-    
-    useEffect(() => {
-        const handleScroll = () => {
-            setCounter(prevCounter => (prevCounter < 90 ? prevCounter + 1 : prevCounter));
-          };
- 
+    const [counterOn, setCounterOn] = useState(false);
 
-      window.addEventListener('scroll', handleScroll);
-
-    //   return () => {
-    //     window.removeEventListener('scroll', handleScroll);
-    //   };
-    }, []);
-  return (
-    <section className='counter'>
-        <div className='container row'>
-            <div className='col-sm-4'>
-                <h1>{counter}</h1>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-            </div>
-            <div className='col-sm-4'>
-                <h1>90</h1>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-            </div>
-            <div className='col-sm-4'>
-                <h1>90</h1>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-            </div>
-        </div>
-    </section>
-  )
+    return (
+        <section className='counter'>
+            <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+                <div className='container row'>
+                    <div className='col-sm-4'>
+                        <h1>
+                            {counterOn && <CountUp start={0} end={90} duration={2} delay={0} />}
+                        </h1>
+                        <h2>Lorem Ipsum</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+                    </div>
+                    <div className='col-sm-4'>
+                        <h1>
+                            {counterOn && <CountUp start={0} end={142} duration={2} delay={0} />}
+                        </h1>
+                        <h2>Lorem Ipsum</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+                    </div>
+                    <div className='col-sm-4'>
+                        <h1>
+                            {counterOn && <CountUp start={0} end={59} duration={2} delay={0} />}
+                        </h1>
+                        <h2>Lorem Ipsum</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+                    </div>
+                </div>
+            </ScrollTrigger>
+        </section>
+    )
 }
 
 export default Counter
